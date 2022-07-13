967e8af8c491df36e8c7b99c661f5fe6bee8ef5f
gudofabi <godofredocpanel@gmail.com>
2022-07-13 13:29:48 +0800

    initial upload

---
+++ frontend/www/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <main>
+
+    <header>
+      <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+      <div class="wrapper">
+
+        <nav class="flex gap-x-4">
+          <RouterLink to="/">Home</RouterLink>
+          <RouterLink to="/about">About</RouterLink>
+        </nav>
+      </div>
+    </header>
+
+    <RouterView />
+  </main>
+</template>
+
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
---
+++ frontend/www/src/views/HomeView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="home-page">
+    <h1 class="text-xl">Home</h1>
+  </div>
+</template>
